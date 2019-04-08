176f8d90ee3ba6244109789ad56c318e586a166b
GAO-Zhenyu <gaozhenyu@zju.edu.cn>
2019-04-08 22:44:38 +0200

    update

--- assignment1/Task1a.py
+++ assignment1/Task1a.py
@@ -1,4 +1,5 @@
 import csv
+import pandas as pd
 
 l = []
 with open("ODI-2019-csv.csv","r") as csv_file:
@@ -7,7 +8,7 @@ with open("ODI-2019-csv.csv","r") as csv_file:
         l.append(row)
 
 #print the number of records
-print (len(l) - 1)
+print ("The number of records: %d" %(len(l) - 1))
 
 str = ''
 #print(l[0])#I don't know why there are two parts in this line???
@@ -15,7 +16,7 @@ print(str.join(l[0]).split(';'))
 
 #print the number of attributes
 number_of_attributes = len(str.join(l[0]).split(';'))
-print(number_of_attributes)
+print ("The number of attributes: %d" %(number_of_attributes))
 
 
 l_fixed = []
@@ -23,9 +24,12 @@ for item in l:
     str =''
     l_item = str.join(item).split(';')
     l_fixed.append(l_item)
+del(l_fixed[0])
 
+l_programme = []
+for item in l_fixed:
+    l_programme.append(item[1])
 
-
-
+print (pd.value_counts(l_programme))
 
 csv_file.close()
